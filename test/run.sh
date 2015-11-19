a304172b74579cfe73996110f51c24520663c38f
Tomas Prerovsky <tomas.prerovsky@googlemail.com>
2015-11-20 00:32:33 +0100

    [FEATURE] New test.

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 echo -e "\e[104mRunning sm4ceps tests\e[0m"
 echo 
-echo -en "Test \e[4msystemparameter.ceps\e[0m:"
+echo -en "Test \e[4msystemparameter.ceps\e[0m                 :"
 ../x86/sm systemparameter.ceps --quiet >systemparameter.log 2>systemparameter.log
 if [ $? -ne 0 ]; then
  echo -e "\e[38;5;196m failed \e[0m"
+else
+ echo -e "\e[38;5;28m passed \e[0m"
+fi
+
+echo -en "Test \e[4mthreads1.ceps\e[0m                        :"
+../x86/sm threads1.ceps --quiet >threads1.log 2>threads1.log
+if [ $? -ne 0 ]; then
+ echo -e "\e[38;5;196m failed \e[0m"
+else
+ echo -e "\e[38;5;28m passed \e[0m"
+fi
+
+echo -en "Test \e[4mplatform_extend_fto_and_alu.ceps\e[0m     :"
+../x86/sm platform_extend_fto_and_alu.ceps --quiet >platform_extend_fto_and_alu.log 2>platform_extend_fto_and_alu.log
+if [ $? -ne 0 ]; then
+ echo -e "\e[38;5;196m failed \e[0m"
+ echo -e "\e[38;5;196m"
+ cat platform_extend_fto_and_alu.log
+ echo -e "\e[0m"
 else
  echo -e "\e[38;5;28m passed \e[0m"
 fi
