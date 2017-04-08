f460c79a87000acc80d7245b2f2c418f1a3bd3f4
ceps <cepsdev@hotmail.com>
2017-04-08 13:19:23 +0200

    [FEATURE] Eclipse project

--- eclipse/Debug/sources.mk
+++ eclipse/Debug/sources.mk
@@ -24,7 +24,12 @@ C_DEPS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 cppgenerator \
+dotgenerator \
+livelog \
 . \
 logging \
+modelling \
 src \
+src/modelling \
+utils \
 
