ed64cf55fa8136d0386c95e4b6679afe7cdccb88
ceps <cepsdev@hotmail.com>
2017-07-07 11:58:29 +0200

    [BUGFIX] CANLayer doc generation script

--- test/fibex/make_canlayer_doc.sh
+++ test/fibex/make_canlayer_doc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-../../x86/sm prelude.ceps $1.xml --no_warn --dump_asciidoc_can_layer > $1.adoc
+../../x86/ceps prelude.ceps $1.xml --no_warn --dump_asciidoc_can_layer > $1.adoc
 asciidoctor $1.adoc
 
