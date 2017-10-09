bcace4f13ec455b7a8f86870ac073a473e4eb6b0
ceps <cepsdev@hotmail.com>
2017-10-09 22:52:19 +0200

    [FEATURE][CEPSCLOUD] Control Panels can be saved now on remote
    Control Panels couldn't be saved.
    
    Using PUSH facility to save control panels.

--- cloud/server/experimental/prototype_ctrlpanel/cepscloud-ui/persistence.js
+++ cloud/server/experimental/prototype_ctrlpanel/cepscloud-ui/persistence.js
@@ -1,5 +1,6 @@
 function build_ctrlpanel_from_json(container, descr) {
     let t = 50;
+    container.set_name(ctrlpanel_info_get_name(descr));
     for (let e of descr.content) {
         let info = ctrlpanel_info_get_widget_descr(e);
         
@@ -28,4 +29,38 @@ function build_ctrlpanel_from_json(container, descr) {
         t += 50;
         //save_widget_info(widget, widget_info, true);
     }
+}
+
+function build_ceps_from_control_panel(ctrl_editor_id) {
+     return `
+ctrlpanel{${ctrlPanelEditor.get_name() ? 'name{"' + ctrlPanelEditor.get_name() + '";};' : ''}`+
+    (() => {
+        let s = "";
+        for (let ch of $("#" + ctrl_editor_id).children()) {
+            let winf = get_widget_info($(ch).attr("id"));
+            function get_type() { if (winf.type.sig) { if (winf.toggle_sig) return "toggle_signal"; if (winf.watch_only) return "watch_signal"; if (winf.do_plot) return "plot_signal"; } return undefined; }
+            function get_assoc_sysstate() { return winf.sig_name }
+            function get_style() {
+                return `width{"${$(ch).css("width")}";};height{"${$(ch).css("height")}";}`; 
+            }
+            if (winf == undefined) continue;
+            if (!$(ch).hasClass("baseWidget")) continue;
+            s += `
+ widget{
+  type{${get_type()};};
+  associated_systemstate{as_symbol("${get_assoc_sysstate()}","Systemstate");};
+  style{${get_style()};};    
+ };` 
+               ;
+            }
+            return s;
+    })()+ `
+};`;
+}
+
+function save_active_control_panel(name) {
+    ctrlPanelEditor.set_name(name);
+    let s = "ctrlpanels{" + build_ceps_from_control_panel("ctrl_panel_editor") + "};";
+    console.log(s);
+    simcore_ws.send(`PUSH ${s}`);
 }
