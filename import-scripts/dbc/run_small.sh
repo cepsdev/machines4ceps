867f0f617c4dec63564ba7b52c945709494f3cf6
ceps <cepsdev@hotmail.com>
2017-11-30 23:01:39 +0100

    [FEATURE] Parameterized DBC Import Correctly Handles Emitting Of Frames
    The cycle attribute was ignored.
    
    Cycle attribute now recognizedby the dbc-lexer and the dbc importer.

--- import-scripts/dbc/run_small.sh
+++ import-scripts/dbc/run_small.sh
@@ -1,1 +1,4 @@
-../../x86/ceps ../common.ceps dbc.ceps.lex frames.dbc config.ceps param_import_dbc.ceps --print_evaluated_input_tree 
+echo
+echo "*** Don't forget to setup a vcan0 CAN interface! ***"
+
+../../x86/ceps ../common.ceps dbc.ceps.lex frames.dbc config.ceps param_import_dbc.ceps driver.ceps --quiet --no_warn
---
+++ import-scripts/dbc/show_tree.sh
@@ -0,0 +1,1 @@
+../../x86/ceps ../common.ceps dbc.ceps.lex frames.dbc config.ceps param_import_dbc.ceps --print_evaluated_input_tree 
