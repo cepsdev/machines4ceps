f5477aed8a9391512152437686a30f8f71846fda
Tomas Prerovsky <tomas.prerovsky@gmail.com>
2018-08-05 10:16:51 +0200

    [BUGFIX][MMS_DEVOPS] Fetching script doesn't work

--- studies/mms_devops/lib/fetch_rollout_plan.sh
+++ studies/mms_devops/lib/fetch_rollout_plan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-#lib/extract_rollouts -h "$ROLLAUT_DB_HOST" -u "$ROLLAUT_DB_USER" -p "$ROLLAUT_DB_PASSWD" -t "2018-06-01 00:00:00" > db_rollout_dump.ceps
+lib/extract_rollouts -h "$ROLLAUT_DB_HOST" -u "$ROLLAUT_DB_USER" -p "$ROLLAUT_DB_PASSWD" -t "2018-06-01 00:00:00" > db_rollout_dump.ceps
