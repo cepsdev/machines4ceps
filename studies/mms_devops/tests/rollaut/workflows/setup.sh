692e08af3ad16ae13bb0cd49d8e427f9ce4e42ae
Tomas Prerovsky <tomas.prerovsky@gmail.com>
2018-11-30 02:57:49 +0100

    [STUDY][MMS_DEVOPS] Linear - Rollout Workflow needs redesign

--- studies/mms_devops/tests/rollaut/workflows/setup.sh
+++ studies/mms_devops/tests/rollaut/workflows/setup.sh
@@ -1,3 +1,4 @@
 ln ../../../../../x86/ceps ceps --symbolic -f
 ln ../../../transformations/rollout2sm.ceps rollout2sm.ceps --symbolic -f
 ln ../../../db_descr/gen.ceps gen.ceps --symbolic -f
+ln ../../../../../../bin/libjenkins4ceps.so libjenkins4ceps.so --symbolic -f
