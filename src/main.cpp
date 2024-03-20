e9889d942d61b797cceedf117ee77ee6262a9245
Tomas Prerovsky <cepsdev@hotmail.com>
2024-03-20 23:08:44 +0100

    Version 0.8.1.3

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 #define VERSION_SM4CEPS_MAJOR "0"
-#define VERSION_SM4CEPS_MINOR "8.1.2"
+#define VERSION_SM4CEPS_MINOR "8.1.3"
 
 vector < string > generated_sql_file_names;
 bool DUMP_PLANTUML_REP_TO_COUT = false;
