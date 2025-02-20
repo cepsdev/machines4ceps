a5ad1cf73048fa47d8004a1ccb9406f689596c56
Tomas Prerovsky <cepsdev@hotmail.com>
2025-02-20 10:18:36 +0100

    Get rid of junk

--- tools/gui/sm4cepsviewer/mdibase.cpp
+++
@@ -1,7 +0,0 @@
-#include "mdibase.h"
-
-int Mdibase::instance_counter = 0;
-
-Mdibase::Mdibase(QWidget * parent, Qt::WindowFlags flags):QMdiSubWindow(parent,flags){
-    this_instance_id = Mdibase::instance_counter++;
-}
--- tools/pltsq/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ -std=c++2a src/main.cpp -I include/ -o pltsq && ./pltsq > test.html && cat test.html
